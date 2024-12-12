aaf3f969f53d7f360f5b2fb956d0176cf8338f28
davidetoscano129 <toscano.davidew@gmail.com>
2024-12-12 17:22:28 +0100

    feat: header&card wip

---
+++ src/components/AppMain.jsx
@@ -0,0 +1,24 @@
+import style from './AppMain.module.css';
+
+function AppMain() {
+    return (
+        <main>
+
+            <div className={style.container}>
+                <section>
+                    <p>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis
+                        placeat dolorem voluptatibus vero laudantium autem omnis architecto
+                        quis consequatur ullam optio est, fuga molestiae sit explicabo
+                        deserunt? Et, pariatur sapiente!
+                    </p>
+                </section>
+            </div>
+        </main>
+
+
+    );
+
+}
+
+export default AppMain;
