d0afbb81a9d6b50b33896483932086d2b49d4b52
davidetoscano129 <toscano.davidew@gmail.com>
2024-12-12 17:48:24 +0100

    feat: footer completed

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -1,3 +1,11 @@
 import style from "./AppFooter.module.css";
 
-function AppFooter() 
+function AppFooter() {
+    return (
+        <footer>
+            <p>© 2024 Toadstool Tales. All rights reserved.</p>
+        </footer>
+    );
+}
+
+export default AppFooter;
