e6a9ac1bdfe66c357aa8aebaed5d76f6b565a484
davidetoscano129 <toscano.davidew@gmail.com>
2024-12-12 17:39:00 +0100

    feat: header&main completed

---
+++ src/components/AppFooter.jsx
@@ -0,0 +1,3 @@
+import style from "./AppFooter.module.css";
+
+function AppFooter() 
--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,24 +1,22 @@
 import style from './AppMain.module.css';
 
 function AppMain() {
+
     return (
         <main>
-
             <div className={style.container}>
                 <section>
+                    <img
+                        src="/images/toadstool.jpg"
+                        alt="Toadstool Image"
+                        className={style.image}
+                    />
                     <p>
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis
-                        placeat dolorem voluptatibus vero laudantium autem omnis architecto
-                        quis consequatur ullam optio est, fuga molestiae sit explicabo
-                        deserunt? Et, pariatur sapiente!
-                    </p>
+                        In the silence of the forest, the toadstool stands as a symbol of balance between nature and mystery. With its unique appearance, it evokes the hidden beauty within the wonders of our world.                    </p>
                 </section>
             </div>
         </main>
-
-
     );
-
 }
 
 export default AppMain;
